fead8f17058b19d7431ae763c7363669979daf07
Saritha3944 <119496247+Saritha3944@users.noreply.github.com>
2023-01-29 23:05:09 +0530

    Add files via upload

---
+++ Javascript Assignment/test3.js
@@ -0,0 +1,5 @@
+var n = prompt("Enter the Number", 10);
+if(n%2 === 0)
+    document.getElementById("demo").innerHTML = "Even Number";
+else
+    document.getElementById("demo").innerHTML = "Odd Number";
